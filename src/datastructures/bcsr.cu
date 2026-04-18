06cb412c49f0f8cca41733e7af7fcbcbce3e98c4
Mehul Goel <mehulg@andrew.cmu.edu>
2026-04-18 11:43:20 -0400

    Added BCSR datastruct

---
+++ src/datastructures/bcsr.cu
@@ -0,0 +1,55 @@
+#include "bcsr.cuh"
+#include <cstring>
+#include <vector>
+
+static constexpr int T = BCSR::TILING;
+
+BCSR::BCSR(const float* host_data, const bool* tile_dense, int M, int N)
+    : M(M), N(N),
+      num_block_rows(M / T),
+      num_block_cols(N / T)
+{
+    int total_blocks = num_block_rows * num_block_cols;
+
+    std::vector<int> h_block_idx(total_blocks, -1);
+    std::vector<int> h_row_ptr(num_block_rows + 1, 0);
+    std::vector<int> h_col_idx;
+    std::vector<float> h_values;
+
+    for (int bi = 0; bi < num_block_rows; bi++) {
+        h_row_ptr[bi] = (int)h_col_idx.size();
+        for (int bj = 0; bj < num_block_cols; bj++) {
+            if (tile_dense[bi * num_block_cols + bj]) {
+                int blk = (int)h_col_idx.size();
+                h_block_idx[bi * num_block_cols + bj] = blk;
+                h_col_idx.push_back(bj);
+
+                int base = blk * T * T;
+                h_values.resize(base + T * T);
+                for (int ti = 0; ti < T; ti++)
+                    memcpy(&h_values[base + ti * T],
+                           &host_data[(bi * T + ti) * N + bj * T],
+                           T * sizeof(float));
+            }
+        }
+    }
+    h_row_ptr[num_block_rows] = (int)h_col_idx.size();
+    nnzb = (int)h_col_idx.size();
+
+    cudaMallocManaged(&block_idx, total_blocks * sizeof(int));
+    cudaMallocManaged(&row_ptr,   (num_block_rows + 1) * sizeof(int));
+    cudaMallocManaged(&col_idx,   nnzb * sizeof(int));
+    cudaMallocManaged(&values,    (size_t)nnzb * T * T * sizeof(float));
+
+    memcpy(block_idx, h_block_idx.data(), total_blocks * sizeof(int));
+    memcpy(row_ptr,   h_row_ptr.data(),   (num_block_rows + 1) * sizeof(int));
+    memcpy(col_idx,   h_col_idx.data(),   nnzb * sizeof(int));
+    memcpy(values,    h_values.data(),    (size_t)nnzb * T * T * sizeof(float));
+}
+
+BCSR::~BCSR() {
+    cudaFree(block_idx);
+    cudaFree(row_ptr);
+    cudaFree(col_idx);
+    cudaFree(values);
+}
